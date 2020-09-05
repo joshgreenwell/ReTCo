0aaddd3c59952b07a0ad183211438a043d9db866
joshgreenwell <joshgreenwell99@gmail.com>
2020-09-05 13:40:47 -0500

    feat(tools): add tools

--- src/main/java/mod/puglove/retco/registries/ModItems.java
+++ src/main/java/mod/puglove/retco/registries/ModItems.java
@@ -1,7 +1,13 @@
 package mod.puglove.retco.registries;
 
 import mod.puglove.retco.ReTCo;
+import mod.puglove.retco.tools.ReTCoItemTier;
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.HoeItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.ShovelItem;
+import net.minecraft.item.SwordItem;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -9,13 +15,42 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class ModItems {
 
   public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, ReTCo.MODID);
-  
-  public static final RegistryObject<Item> DIMENSIONAL_RESIDUE = ITEMS.register("dimensional_residue", () -> new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
-  public static final RegistryObject<Item> DIMENSIONAL_INGOT = ITEMS.register("dimensional_ingot", () -> new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
-  public static final RegistryObject<Item> ENHANCED_DIMENSIONAL_INGOT = ITEMS.register("enhanced_dimensional_ingot", () -> new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
-  
-  public static final RegistryObject<Item> FORMING_TOOL = ITEMS.register("forming_tool", () -> new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
 
-  public static final RegistryObject<Item> DECONSTRUCTED_DIMENSIONAL_ENERGY_SIPHONER = ITEMS.register("deconstructed_dimensional_energy_siphoner", () -> new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
+  // Ingredients
+  public static final RegistryObject<Item> DIMENSIONAL_RESIDUE = ITEMS.register("dimensional_residue",
+      () -> new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
+  public static final RegistryObject<Item> DIMENSIONAL_INGOT = ITEMS.register("dimensional_ingot",
+      () -> new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
+  public static final RegistryObject<Item> ENHANCED_DIMENSIONAL_INGOT = ITEMS.register("enhanced_dimensional_ingot",
+      () -> new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
+  public static final RegistryObject<Item> DECONSTRUCTED_DIMENSIONAL_ENERGY_SIPHONER = ITEMS.register(
+      "deconstructed_dimensional_energy_siphoner",
+      () -> new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
 
+  // Custom tools
+  public static final RegistryObject<Item> FORMING_TOOL = ITEMS.register("forming_tool",
+      () -> new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
+
+  // Tools
+  public static final RegistryObject<SwordItem> DIMENSIONAL_SWORD = ITEMS.register("dimensional_sword",
+      () -> new SwordItem(ReTCoItemTier.DIMENSIONAL, 5, -2.2f,
+          new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
+
+  public static final RegistryObject<AxeItem> DIMENSIONAL_AXE = ITEMS.register("dimensional_axe",
+      () -> new AxeItem(ReTCoItemTier.DIMENSIONAL, 2, -2.4f,
+          new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
+
+  public static final RegistryObject<PickaxeItem> DIMENSIONAL_PICKAXE = ITEMS.register("dimensional_pickaxe",
+      () -> new PickaxeItem(ReTCoItemTier.DIMENSIONAL, 1, -2.6f,
+          new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
+
+  public static final RegistryObject<ShovelItem> DIMENSIONAL_SHOVEL = ITEMS.register("dimensional_shovel",
+      () -> new ShovelItem(ReTCoItemTier.DIMENSIONAL, 0, -2.6f,
+          new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
+
+  public static final RegistryObject<HoeItem> DIMENSIONAL_HOE = ITEMS.register("dimensional_hoe",
+      () -> new HoeItem(ReTCoItemTier.DIMENSIONAL, -2.6f,
+          new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
+
+  // Armor
 }
---
+++ src/main/java/mod/puglove/retco/tools/ReTCoItemTier.java
@@ -0,0 +1,63 @@
+package mod.puglove.retco.tools;
+
+import java.util.function.Supplier;
+
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.crafting.Ingredient;
+
+import mod.puglove.retco.registries.ModItems;
+
+public enum ReTCoItemTier implements IItemTier {
+  
+  DIMENSIONAL(4, 2000, 9.0f, 2.0f, 12, () -> {
+    // Can be any item that uses this tier material
+    return Ingredient.fromItems(ModItems.DIMENSIONAL_SWORD.get());
+  });
+
+  private final int harvestLevel;
+  private final int maxUses;
+  private final float efficiency;
+  private final float attackDamage;
+  private final int enchantability;
+  private final Supplier<Ingredient> repairMaterial;
+
+  ReTCoItemTier(int harvestLevel, int maxUses, float efficiency, float attackDamage, int enchantability, Supplier<Ingredient> repairMaterial) {
+    this.harvestLevel = harvestLevel;
+    this.maxUses = maxUses;
+    this.efficiency = efficiency;
+    this.attackDamage = attackDamage;
+    this.enchantability = enchantability;
+    this.repairMaterial = repairMaterial;
+  }
+
+  @Override
+  public int getMaxUses() {
+    return maxUses;
+  }
+
+  @Override
+  public float getEfficiency() {
+    return efficiency;
+  }
+
+  @Override
+  public float getAttackDamage() {
+    return attackDamage;
+  }
+
+  @Override
+  public int getHarvestLevel() {
+    return harvestLevel;
+  }
+
+  @Override
+  public int getEnchantability() {
+    return enchantability;
+  }
+
+  @Override
+  public Ingredient getRepairMaterial() {
+    return repairMaterial.get();
+  }
+  
+}
